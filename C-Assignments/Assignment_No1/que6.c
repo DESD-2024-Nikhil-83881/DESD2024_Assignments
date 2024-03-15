aa6d611d7aca85cabe6d7f02b66f678265ee0bf7
NikhilBorate <nikhilborate1998@gmail.com>
2024-03-15 23:22:36 +0530

    ASSIGN1

---
+++ C-Assignments/Assignment_No1/que6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+int i,n;
+printf("Enter the no :");
+scanf("%d",&n);
+
+for(i = 1;i<=10;i++){
+
+int  a = i*n;
+
+printf("%d\n",a);
+
+
+}
+return 0;
+
+
+}
---
+++ C-Assignments/Assignment_No1/que9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+float far,cel,a;
+a =9.0/5.0;
+printf("The temperature is :\n");
+scanf("%f",&cel);
+
+
+
+far = (cel*a)+32;
+
+printf("The farenheight temperature is %f",far);
+
+return 0;
+}
